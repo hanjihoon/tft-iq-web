7a3a0009876abbe590cde2fe19c74a6096c2e484
han jihoon <showmethefrank@gmail.com>
2026-07-02 16:43:16 +0900

    vercel 분석 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './tft-iq-quiz-card.jsx'; // ← 이 줄
+
+
+// 1. 아래 패키지를 import 하세요
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+// 2. 앱 실행 시 분석 기능을 활성화합니다
+injectSpeedInsights();
+inject();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
